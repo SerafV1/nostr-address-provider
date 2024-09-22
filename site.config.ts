b8fb1bbadc8030246799da1fc2873437c6e33fd8
Seraf <milerbrane@gmail.com>
2024-09-22 18:35:46 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default {
 
   // your donation LNURL
   donationLNUrl:
-    "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7JMPVDV9YCC4FVREX",
+    "seraf@getalby.com",
 
   // name of your MongoDB database
   dbName: "users",
